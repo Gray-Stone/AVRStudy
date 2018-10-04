128916d973613c535a8d9f07e7c97db5b48165ce
Leo Gray <leograyc@gmail.com>
2018-10-04 04:07:37 -0400

    Initial commit

---
+++ ADC input/main.c
@@ -0,0 +1,20 @@
+/*
+ * ADC input.c
+ *
+ * Created: 2018/1/5 16:57:20
+ * Author : leogr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
+
